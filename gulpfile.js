49a78a861b9de22a933389d9b332645c957b5b9a
Tifco Hotel Group <45363821+Tifco-Hotel-Group@users.noreply.github.com>
2020-02-19 15:57:40 +0000

    new update

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+}
+/*
+    -- Top Level Functions --
+    gulp.task - Define tasks
+    gulp.src - Point to files to use
+    gulp.dest - Points to folder to output to
+    gulp.watch - Watch files and folders for changes
+*/
+
+// Logs Message
+
+exports.default = defaultTask
